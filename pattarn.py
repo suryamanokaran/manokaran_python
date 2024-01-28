f5ad3f8484470a7afb972ea2836f8e56b9511af9
suryamanokaran <suryamanokaran2001@gmail.com>
2024-01-28 14:10:37 +0530

    first commit

---
+++ pattarn.py
@@ -0,0 +1,12 @@
+print("11 matrix")
+for n in range(1,9,1):
+    print(n,"*","11=",n*11)
+
+print("multiplication of 5")
+for x in range (1,9,1):
+    print(x,"^","5=",5**x)
+
+a=0
+for y in range(1,11,1):
+    a=a+y
+print(a)
